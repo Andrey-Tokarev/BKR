64e532a009a0e1c31a997c00dcb426dd0985e0f8
Andrey-Tokarev <andrei.tokarev@gmail.com>
2016-08-17 11:02:17 -0700

    finally removed all cloning and removing relying instead on zero road population. in that case no need to transfer roads through arguments every time, made it a class field

--- src/leftEdges/BearKRoads.java
+++ src/leftEdges/BearKRoads.java
@@ -1,85 +1,54 @@
 package leftEdges;
 
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class BearKRoads {
 	int[] pops;  // cities populations
 	ArrayList<ArrayList<Road>> adjL;
+	ArrayList<Road> roads;
 	
-	public class Road implements Comparable<Road>{
+	public class Road {
 		int i;
 		int j;
-		
+
 		Road(int i, int j) {
 			this.i = i;
 			this.j = j;
 		}
-		
-		Road(Road road) {
-			this.i = road.i;
-			this.j = road.j;
-		}
-		
-		public int roadPop() {  // road population (which is sum of the connected cities 
+
+		public int roadPop() { // road population (sum of the connected cities)
 			return pops[i] + pops[j];
 		}
-		
-		@Override
-		public int compareTo(Road o) {
-			return this.roadPop() - o.roadPop();
-		}
-	   
-		@Override
-	    public boolean equals(Object object)
-	    {
-	        boolean sameSame = false;
 
-	        if (object != null && object instanceof Road)
-	        {
-	            sameSame = (this.i == ((Road) object).i && this.j == ((Road) object).j);
-	        }
-
-	        return sameSame;
-	    }	}
-	
-	private ArrayList<Road> cloneRoads(ArrayList<Road> roads) {
-		ArrayList<Road> cloneRoads = new ArrayList<Road>(roads.size());
-		for(Road road: roads) {
-			cloneRoads.add(new Road(road));
-		}
-		return cloneRoads;
 	}
-
-	private Road getMaxRoad(ArrayList<Road> roads) {
+	
+	private Road getMaxRoad() {
 		Road maxRoad = roads.get(0);
-		for(Road road: roads) {
-			if(road.roadPop() > maxRoad.roadPop()) {
+		for (Road road : roads) {
+			if (road.roadPop() > maxRoad.roadPop()) {
 				maxRoad = road;
 			}
 		}
 		return maxRoad;
 	}
-	
-	private int workMaxHappy(ArrayList<Road> roads, int K) {
+
+	private int workMaxHappy(int K) {
 		// firstly with top
-		if(roads.isEmpty() || K == 0) {
+		if (K == 0) {
 			return 0;
 		}
-		Road topRoad = getMaxRoad(roads);
-		// checking all roads connected to the left city of the top road (including top itself)
-		int roadPop = 0;
+		Road topRoad = getMaxRoad();
+		// checking all roads connected to the left city of the top road
+		// (including top itself)
 		ArrayList<Road> leftRoads = adjL.get(topRoad.i);
 		int res = 0;
-		for(Road leftRoad: leftRoads) {
-			if(roads.contains(leftRoad)) {
-				ArrayList<Road> copyRoads = cloneRoads(roads);
-				copyRoads.remove(leftRoad);
+		for (Road leftRoad : leftRoads) {
+			int roadPop = leftRoad.roadPop();
+			if (roadPop != 0) {
 				int prevIPop = pops[leftRoad.i];
 				int prevJPop = pops[leftRoad.j];
-				roadPop = leftRoad.roadPop();
 				pops[leftRoad.i] = pops[leftRoad.j] = 0;
-				res = Math.max(res, roadPop + workMaxHappy(copyRoads, K - 1));
+				res = Math.max(res, roadPop + workMaxHappy(K - 1));
 				pops[leftRoad.i] = prevIPop;
 				pops[leftRoad.j] = prevJPop;
 			}
@@ -93,7 +62,7 @@ public class BearKRoads {
 		for(int i = 0; i < x.length; ++i) {
 			adjL.add(new ArrayList<Road>());
 		}
-		ArrayList<Road> roads = new ArrayList<Road>(); 
+		roads = new ArrayList<Road>(); 
 		int M = a.length;
 		for(int l = 0; l < M; ++l) {
 			Road road = new Road(a[l], b[l]);
@@ -101,7 +70,7 @@ public class BearKRoads {
 			(adjL.get(a[l])).add(road);
 			(adjL.get(b[l])).add(road);
 		}
-		return workMaxHappy(roads, K);
+		return workMaxHappy(K);
 	}
 
 		
